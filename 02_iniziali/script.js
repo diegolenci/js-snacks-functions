890bcc8fa45f24bbada8230252dc3adbfa406a55
diegolenci <lencidiego0@gmail.com>
2025-01-17 10:27:12 +0100

    Correzione esercizi

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,20 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola 
+dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+const lettereNames = []
 
 // Dichiara la funzione qui.
+function letturaArray(){
+    for ( let i=0; i < names.length; i++)
+        console.log(names[i].charAt(0))
+        lettereNames.push( names.charAt(0))
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+letturaArray(names)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log( lettereNames )
--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,28 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vocali = aeiouAEIOU
+let contatoreVocali = 0
+let arrayVocali = []
 
 // Dichiara la funzione qui.
+function letturaStringa(stringa){
 
+    for ( let i=0; i < stringa.length; i++){
+
+        for ( let k=0; k < vocali.length; k++){
+            if( stringa[i] === vocali[k] ){
+                contatoreVocali++
+                arrayVocali.push( vocali[k] )
+            }
+        }
+
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+letturaStringa(word)
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log( contatoreVocali, arrayVocali)
